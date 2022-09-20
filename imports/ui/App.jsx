30c43ee8cb3a92a19f2c7d1eb3fed4b32b8d3d25
Kristina <you@example.com>
2022-09-20 22:26:14 +0200

    first commit

---
+++ imports/ui/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import {ContactForm} from '../ui/ContactForm';
+import {ContactList} from '../ui/ContactList';
+
+export const App = () => (
+  <div>
+    <h1>meteor walet</h1>
+    <ContactForm/>
+    <ContactList/>
+  </div>
+);
